ef5b1d07c6de7e53469ac5dc7cfb18740cae268f
Thomas Humphrey <thumphrey9123@gmail.com>
2019-12-31 04:04:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,7 @@
 def reverse_each_word(string)
   arr = string.split(" ")
   reversed = []
-  i=0 
-  while i<arr.length do
-    reversed << arr[i].reverse
-    
-    i+=1
-  end
+  arr.collect {|x| x.reverse}
   string = reversed.join(" ")
 string
 end
